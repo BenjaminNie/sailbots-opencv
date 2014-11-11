a1ed8a94e31d41e8fcaf088ecac5fb52e6688f78
derek-lun <dereklun@gmail.com>
2014-11-10 19:16:23 -0800

    first commit

---
+++ main.py
@@ -0,0 +1,12 @@
+import cv2
+import math
+
+img = cv2.imread("IR_0423.jpg")
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+edges = cv2.Canny(gray, 80, 120)
+lines = cv2.HoughLinesP(edges, 1, math.pi/2, 2, None, 30, 1);
+for line in lines[0]:
+    pt1 = (line[0],line[1])
+    pt2 = (line[2],line[3])
+    cv2.line(img, pt1, pt2, (0,0,255), 3)
+cv2.imwrite("IR_0423-2.jpg", img)
